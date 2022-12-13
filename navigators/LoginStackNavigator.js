70835005dbd16bbb163b847dd8eb1f50199a4916
Daniel Cervera <danielcervera@Daniels-MacBook-Air.local>
2022-12-12 18:40:45 -0600

    Login screen added

--- navigators/LoginStackNavigator.js
+++ navigators/LoginStackNavigator.js
@@ -6,7 +6,9 @@ import BottomTabNavigator from './BottomTabNavigator';
 const Stack = createNativeStackNavigator();
 
 const LoginStackNavigator = () => (
-  <Stack.Navigator initialRouteName="Login">
+  <Stack.Navigator
+    initialRouteName="Login"
+    screenOptions={{ headerShown: false }}>
     <Stack.Screen name="Login" component={LoginScreen} />
     <Stack.Screen name="HomeNavigator" component={BottomTabNavigator} />
   </Stack.Navigator>
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,21 +1,55 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Button, TextInput } from 'react-native-paper';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import { Button, TextInput, Text } from 'react-native-paper';
 
+import CocodrileSvg from '../images/CocodrileSvg';
+
+/**
+ * TODO:
+ * Create components for: TextInput and Button
+ * Remove all inline styles
+ */
 const LoginScreen = ({ navigation }) => {
   return (
-    <View
-      style={{
-        borderBottomColor: '#000000',
-        borderBottomWidth: 1,
-      }}>
-      <TextInput label="Email" mode="outlined" />
-      <TextInput label="Password" mode="outlined" />
+    <View style={styles.screenContainer}>
+      <View style={{ height: 147, width: 110 }}>
+        <CocodrileSvg />
+      </View>
+      <Text variant="titleLarge" style={{ fontWeight: 'bold' }}>
+        Welcome to Diginventory!
+      </Text>
+      <TextInput
+        label="Email"
+        mode="outlined"
+        style={{ width: '100%', marginTop: 24 }}
+        outlineStyle={{ borderRadius: 8 }}
+      />
+      <TextInput
+        label="Password"
+        mode="outlined"
+        style={{ width: '100%', marginTop: 8 }}
+        outlineStyle={{ borderRadius: 8 }}
+      />
+      <View style={{ width: '100%', marginTop: 24 }}>
+        <Button
+          onPress={() => navigation.navigate('HomeNavigator')}
+          mode="contained">
+          Log in
+        </Button>
+      </View>
       <Button onPress={() => navigation.navigate('HomeNavigator')}>
-        Press me
+        Forgot your password
       </Button>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+    alignItems: 'center',
+    paddingTop: '20%',
+    paddingHorizontal: 24,
+  },
+});
 export default LoginScreen;
